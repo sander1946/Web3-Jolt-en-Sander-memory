10e276aa5c91c8524b4cd6612a548ed8eab4af7d
sander1946 <71840186+sander1946@users.noreply.github.com>
2025-05-07 14:55:28 +0200

    I'm pushing goddammit

--- public/index.js
+++ public/index.js
@@ -10,39 +10,54 @@ async function updateBoardSize(inputSelector) {
         return;
     }
 
-    var cards = [];
+    var cards_unshuffled = [];
     for (let i = 0; i < catJson.length; i++) {
-        cards.push(catJson[i].id);
-        cards.push(catJson[i].id); // Duplicate each image ID for the matching game
+        cards_unshuffled.push(catJson[i].id);
+        cards_unshuffled.push(catJson[i].id); // Duplicate each image ID for the matching game
     }
+
+    console.log(cards_unshuffled); // TODO: remove this line in production
+
+    // Shuffle the cards array to randomize the order of images
+    // cards.sort(() => Math.random() - 0.1); // TODO: find better random sort algorithm
+
+    cards = cards_unshuffled
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
 
     console.log(cards); // TODO: remove this line in production
 
-    // Shuffle the cards array to randomize the order of images
-    cards.sort(() => Math.random() - 0.5);
-
     cards.forEach(function (cardId, index) {
+        const imgUrl = `https://cataas.com/cat/${cardId}`;
+
         const card = document.createElement('div');
         card.classList.add('card');
         card.setAttribute('data-card-id', cardId);
         card.setAttribute('data-card-status', 'closed');
+        card.setAttribute('imgurl', imgUrl);
 
-        const cardImg = document.createElement('img');
-        const imgUrl = `https://cataas.com/cat/${cardId}`;
-        cardImg.setAttribute('src', imgUrl);
-        cardImg.setAttribute('alt', `catId ${cardId}`);
-        cardImg.setAttribute('style', "display: none;");
+        const cardImgBackground = document.createElement('div');
+        cardImgBackground.setAttribute('style', `background-image: var(--card-img-closed-background-img);`);
+        cardImgBackground.classList.add('card-image');
+
+        const cardImg = document.createElement('div');
+        cardImg.setAttribute('style', `display: none; background-image: url('${imgUrl}');`);
         cardImg.classList.add('card-image');
 
-        card.appendChild(cardImg);
+        cardImgBackground.appendChild(cardImg);
+        card.appendChild(cardImgBackground);
         card.addEventListener('click', function() {
             const cardStatus = card.getAttribute('data-card-status');
+            const imgUrl = card.getAttribute('imgurl');
             if (cardStatus === 'closed') {
                 card.setAttribute('data-card-status', 'open');
-                cardImg.setAttribute('style', "display: block;");
+                cardImg.setAttribute('style', `display: block; background-image: url('${imgUrl}');`);
+                cardImgBackground.setAttribute('style', `background-color: var(--card-img-background-color);`);
             } else {
                 card.setAttribute('data-card-status', 'closed');
-                cardImg.setAttribute('style', "display: none;");
+                cardImg.setAttribute('style', `display: none; background-image: url('${imgUrl}');`);
+                cardImgBackground.setAttribute('style', `background-image: var(--card-img-closed-background-img);`);
             }
         });
         board.appendChild(card);
