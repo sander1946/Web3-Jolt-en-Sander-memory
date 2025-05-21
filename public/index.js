ceded2339ce53eea3d7983208ef9a554c49f16d2
sander1946 <71840186+sander1946@users.noreply.github.com>
2025-05-21 15:39:37 +0200

    added a popup

--- public/index.js
+++ public/index.js
@@ -22,3 +22,26 @@ document.addEventListener('DOMContentLoaded', () => {
         input.addEventListener('change', () => updateCardColor(input));
     });
 });
+
+
+// Get the modal
+var modal = document.getElementById("popup");
+
+// // Get the button that opens the modal
+// let btn = document.getElementById("myBtn"); 
+// btn.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
