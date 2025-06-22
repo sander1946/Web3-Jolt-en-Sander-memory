ce03f4ef35423a7ed1bc252855a958be6bebc4ea
sander1946 <71840186+sander1946@users.noreply.github.com>
2025-06-22 10:58:49 +0200

    added more example components

--- memory-admin/src/app/app.ts
+++ memory-admin/src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Navbar } from './navbar/navbar';
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './templates/app.html',
-  styleUrl: './static/app.css'
+  imports: [RouterOutlet, Home, Navbar, Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
 })
 export class App {
   protected title = 'memory-admin';
