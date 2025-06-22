2a0d625680358d5803467941e657cfccdfa3dae8
sander1946 <71840186+sander1946@users.noreply.github.com>
2025-06-22 14:17:41 +0200

    cleanup

--- memory-admin/src/app/home/home.ts
+++ memory-admin/src/app/home/home.ts
@@ -1,20 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HousingLocation } from '../housing-location/housing-location';
-import { HousingLocationInfo } from '../housinglocation';
-import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, HousingLocation],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
-  housingLocationList: HousingLocationInfo[] = [];
-  housingService: HousingService = inject(HousingService);
 
-  constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-  }
 }
