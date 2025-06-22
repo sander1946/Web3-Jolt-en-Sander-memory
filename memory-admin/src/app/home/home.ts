635374d51880bc1bcdb25f6d3f7b3e4deda537be
sander1946 <71840186+sander1946@users.noreply.github.com>
2025-06-22 11:15:54 +0200

    added example component with data sharing

--- memory-admin/src/app/home/home.ts
+++ memory-admin/src/app/home/home.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {HousingLocation} from '../housing-location/housing-location';
+import {HousingLocationInfo} from '../housinglocation';
 
 @Component({
   selector: 'app-home',
-  imports: [HousingLocation],
+  imports: [CommonModule, HousingLocation],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class Home {
-
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocation: HousingLocationInfo = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
